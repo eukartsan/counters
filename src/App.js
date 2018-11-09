7e5012cb25d5d56e59da899549e2ae93d8eed4cf
eukartsan <eugenekartsan@gmail.com>
2018-11-09 19:15:56 +0200

    add button onIncrement and onDecrement

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
-
 
 // state data for 3 counters
 const data = [
-  { id: 1, value: 0 },
-  { id: 2, value: 0 },
-  { id: 3, value: 0 }
+  {
+    id: 1,
+    value: 0
+  }, {
+    id: 2,
+    value: 0
+  }, {
+    id: 3,
+    value: 0
+  }
 ];
 
 class Counter extends Component {
+  constructor() {
+    super();
+    this.state = {
+      value: 0
+    };
+  }
+
+  onDecrement = () => {
+
+    this.setState({
+      value: this.state.value - 1
+    });
+  }
+
+  onIncrement = () => {
+    this.setState({
+      value: this.state.value + 1
+    });
+  }
+
   render() {
-    const { value } = this.props;
-    return (
-      <div className="counter">
-        <b>{value}</b>
-        <div className="counter-controls">
-          <button className="button is-danger is-small">-</button>
-          <button className="button is-success is-small">+</button>
-        </div>
+    const {value} = this.props;
+    return (<div className="counter">
+      <b>{this.state.value}</b>
+      <div className="counter-controls">
+        <button onClick={this.onDecrement} className="button is-danger is-small">-</button>
+        <button onClick={this.onIncrement} className="button is-success is-small">+</button>
       </div>
-    );
+    </div>);
   }
 }
 
@@ -30,16 +54,9 @@ class App extends Component {
   }
 
   render() {
-    return (
-      <div>
-        {data.map(counter => (
-          <Counter
-            key={counter.id}
-            value={counter.value}
-            />
-        ))}
-      </div>
-    );
+    return (<div>
+      {data.map(counter => (<Counter key={counter.id} value={counter.value}/>))}
+    </div>);
   }
 }
 
